4e3e3191703aa19ea240d14d7158250bef219785
weii41392 <weii41392@gmail.com>
2020-05-08 22:58:11 +0800

    Fixed typo

--- Lab3/ALU_Ctrl.v
+++ Lab3/ALU_Ctrl.v
@@ -23,7 +23,7 @@ assign	ALU_Ctrl_o = (ALUOp==2'b00)?			4'b0010:(		// ld, sd
 					 (instr[3:0]==4'b0111)?		4'b0000:(		// and
 					 (instr[3:0]==4'b1000)?		4'b0110:(		// sub
 					 (instr[3:0]==4'b1101)?		4'b0101:(		// sra
-					 							0))))))))))		// else
+												0))))))))));	// else
 
 
 endmodule
