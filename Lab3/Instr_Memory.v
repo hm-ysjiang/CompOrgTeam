fec1a9a53d975cb5affe6c8f6686da434bcfbc2c
hm-ysjiang <hmysjiang.git@gmail.com>
2020-05-13 19:44:47 +0800

    Add header info

--- Lab3/Instr_Memory.v
+++ Lab3/Instr_Memory.v
@@ -1,6 +1,6 @@
 /***************************************************
-Student Name: 
-Student ID: 
+Student Name: 黃偉傑、江岳勳
+Student ID: 0716222、0716214
 ***************************************************/
 
 `timescale 1ns/1ps
@@ -16,7 +16,7 @@ reg		[32-1:0] instruction_file	[0:31];
 initial begin
     for ( i=0; i<32; i=i+1 )
 	    instruction_file[i] = 32'b0;
-    $readmemb("CO_test_data3.txt", instruction_file); 
+    $readmemb("CO_test_data1.txt", instruction_file); 
 end
 
 assign	instr_o = instruction_file[addr_i/4];
