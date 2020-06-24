153f8cf7c6d60f66d1e77882a651685fd049a8bc
hm-ysjiang <hmysjiang.git@gmail.com>
2020-06-24 17:36:45 +0800

    LRU

--- Lab6/direct_mapped_cache.cpp
+++ Lab6/direct_mapped_cache.cpp
@@ -1,26 +1,26 @@
-#include <vector>
-#include <iostream>
-#include <iomanip>
+#include <cmath>  // log2()
 #include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <sstream>
-#include <cmath> // log2()
+#include <vector>
 using namespace std;
 
-#define CACHE_SIZE (4) // 4K
+#define CACHE_SIZE (4)  // 4K
 #define BLOCK_SIZE (16)
 
-typedef string INPUT_T; // change different type may not work
+typedef string INPUT_T;  // change different type may not work
 vector<INPUT_T> D_Cache;
 vector<INPUT_T> I_Cache;
 
-int hit_num=0, miss_num=0, access_num=0;
-int tag_digit = 8 - (int) log2(CACHE_SIZE*1024/BLOCK_SIZE) - (int) log2(BLOCK_SIZE),
-    index_digit = (int) log2(CACHE_SIZE*1024/BLOCK_SIZE);
+int hit_num = 0, miss_num = 0, access_num = 0;
+int tag_digit = 8 - (int)log2(CACHE_SIZE * 1024 / BLOCK_SIZE) - (int)log2(BLOCK_SIZE),
+    index_digit = (int)log2(CACHE_SIZE * 1024 / BLOCK_SIZE);
 
-typedef struct element{
+typedef struct element {
     int tag, index;
-    element(){}
-    element(string in){
+    element() {}
+    element(string in) {
         stringstream ss(in);
         ss << hex << in.substr(0, tag_digit);
         ss >> tag;
@@ -29,56 +29,58 @@ typedef struct element{
     }
 } element;
 
-void input(void){
+void input(void) {
     ifstream ifs;
-    if(ifs.is_open()) throw("ifs has been already opened.\n");
+    if (ifs.is_open())
+        throw("ifs has been already opened.\n");
     ifs.open("DCACHE.txt", ifstream::in);
 
     INPUT_T input;
-    while(ifs >> input) D_Cache.push_back(input);
+    while (ifs >> input)
+        D_Cache.push_back(input);
     ifs.close();
 
-
-    if(ifs.is_open()) throw("ifs has been already opened.\n");
+    if (ifs.is_open())
+        throw("ifs has been already opened.\n");
     ifs.open("ICACHE.txt", ifstream::in);
-    while(ifs >> input) I_Cache.push_back(input);
+    while (ifs >> input)
+        I_Cache.push_back(input);
     ifs.close();
 
     return;
 }
-void memory_access(void){
-    vector<pair<bool, element>> map(CACHE_SIZE*1024/BLOCK_SIZE, pair<bool, element>(false, element()));
+void memory_access(void) {
+    vector<pair<bool, element>> map(CACHE_SIZE * 1024 / BLOCK_SIZE, pair<bool, element>(false, element()));
 
-    for(int i=0; i<I_Cache.size(); i++){
+    for (int i = 0; i < I_Cache.size(); i++) {
         element access(I_Cache[i]);
-        if(map[access.index].first==false){ // compulsory miss
+        if (map[access.index].first == false) {  // compulsory miss
             map[access.index].second = access;
             map[access.index].first = true;
             miss_num++;
         }
-        else{
-            if(map[access.index].second.tag==access.tag){
+        else {
+            if (map[access.index].second.tag == access.tag) {
                 hit_num++;
             }
-            else{
-                map[accee.index].second.tag = access.tag;
+            else {
+                map[access.index].second.tag = access.tag;
                 miss_num++;
             }
         }
         access_num++;
     }
-
 }
-void print_result(void){
+void print_result(void) {
     float hit_rate = hit_num / access_num, miss_rate = miss_num / access_num;
-    cout << "Cache_size: " << CACHE_SIZE << '\n' <<
-            "Block_size: " << BLOCK_SIZE << '\n' <<
-            "Hit rate: " << fixed << setprecision(2) << hit_rate  << "% (" << hit_num  << "),  " <<
-            "Miss rate: " << miss_rate << "% (" << miss_num << ")\n";
+    cout << "Cache_size: " << CACHE_SIZE << '\n'
+         << "Block_size: " << BLOCK_SIZE << '\n'
+         << "Hit rate: " << fixed << setprecision(2) << hit_rate << "% (" << hit_num << "),  "
+         << "Miss rate: " << miss_rate << "% (" << miss_num << ")\n";
     return;
 }
 
-int main(void){
+int main(void) {
     input();
     memory_access();
     print_result();
--- Lab6/set_associative_cache.cpp
+++ Lab6/set_associative_cache.cpp
@@ -1,24 +1,70 @@
-#include <iostream>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+#define BLOCK_SIZE 64
+
 using namespace std;
 
-#define N (1)
-#define CACHE_SIZE (32) // 32K
-#define BLOCK_SIZE (64)
+vector<uint32_t> _data;
+int siz[] = {1, 2, 4, 8, 16, 32, 64},
+    associa[] = {1, 2, 4, 8};
 
-
-void print_result(const float &hit_rate, const int &hit_num, const float &miss_rate, const int &miss_num){
-    cout << N << "-N Way\n" <<
-            "Cache_size: " << CACHE_SIZE << '\n' <<
-            "Block_size: " << BLOCK_SIZE << '\n' <<
-            "Hit rate: " << fixed << setprecision(2) << hit_rate  << "% (" << hit_num  << "),  " <<
-            "Miss rate: " << miss_rate << "% (" << miss_num << ")\n";
-    return;
+void readData() {
+    ifstream in("LRU.txt", ios::in);
+    uint32_t t;
+    while (in >> hex >> t) {
+        _data.push_back(t);
+    }
+    in.close();
 }
 
-int main(void){
+void test(int cache_size_K, int associa_WAY) {
+    int block_count = cache_size_K * 1024 / BLOCK_SIZE;
+    int set_count = block_count / associa_WAY;
+    vector<vector<uint32_t>> cache(set_count, vector<uint32_t>());
+    int hit = 0, miss = 0;
+    for (int i = 0; i < _data.size(); i++) {
+        uint32_t t = _data[i];
+        t /= BLOCK_SIZE;
+        uint32_t cache_idx = t % set_count, tag = t / set_count;
+        bool done = false;
+        for (int j = 0; j < cache[cache_idx].size(); j++) {
+            if (cache[cache_idx][j] == tag) {
+                cache[cache_idx].erase(cache[cache_idx].begin() + j);
+                cache[cache_idx].push_back(tag);
+                done = true;
+                hit++;
+                break;
+            }
+        }
+        if (!done && cache[cache_idx].size() < associa_WAY) {
+            miss++;
+            cache[cache_idx].push_back(tag);
+            done = true;
+        }
+        else if (!done) {
+            miss++;
+            cache[cache_idx].erase(cache[cache_idx].begin());
+            cache[cache_idx].push_back(tag);
+        }
+    }
+    float hit_rate = (float)(hit) / (float)(hit + miss) * 100;
+    cout << associa_WAY << "-Way" << endl
+         << "Cache_size: " << cache_size_K << 'K' << endl
+         << "Block_size: " << BLOCK_SIZE << endl
+         << "Hit rate: " << fixed << setprecision(2) << hit_rate << "% (" << hit << "), Miss rate: " << (100.0F - hit_rate) << "% (" << miss << ")" << endl;
+}
 
-    print_result();
+int main(void) {
+    readData();
+    for (int i = 0; i < 4; i++) {
+        cout << "-------------------------\n";
+        for (int j = 0; j < 7; j++) {
+            test(siz[j], associa[i]);
+        }
+        cout << "-------------------------\n";
+    }
     return 0;
 }
