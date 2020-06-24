362136bb8b4765f5cc78b3b9053070b5b97b043e
weii41392 <weii41392@gmail.com>
2020-06-24 17:14:21 +0800

    Print func update

--- Lab6/direct_mapped_cache.cpp
+++ Lab6/direct_mapped_cache.cpp
@@ -4,13 +4,12 @@
 #include <fstream>
 using namespace std;
 
-#define CACHE_SIZE (16) // 4K
-#define BLOCK_SIZE (64)
+int cache[] = {4, 16, 64, 256},
+    block[] = {16, 32, 64, 128, 256};
 
 vector<unsigned int> D_Cache;
 vector<unsigned int> I_Cache;
 
-int block_num = CACHE_SIZE * 1024 / BLOCK_SIZE;
 
 typedef struct element{
     bool valid;
@@ -25,7 +24,6 @@ void input(void){
 
     unsigned int input;
     while(ifs >> hex >> input){
-        input /= BLOCK_SIZE;
         D_Cache.push_back(input);
     }
     ifs.close();
@@ -34,27 +32,27 @@ void input(void){
     if(ifs.is_open()) throw("ifs has been opened already.\n");
     ifs.open("ICACHE.txt", ifstream::in);
     while(ifs >> hex >> input){
-        input /= BLOCK_SIZE;
         I_Cache.push_back(input);
     }
     ifs.close();
 
     return;
 }
-void print_result(const int &hit_num, const int &miss_num){
+void print_result(const int &hit_num, const int &miss_num, const int &cache_size, const int &block_size){
     float hit_rate = (float) hit_num / (float)(hit_num + miss_num), miss_rate = (float) miss_num / (float)(hit_num + miss_num);
-    cout << "Cache_size: " << CACHE_SIZE << '\n' <<
-            "Block_size: " << BLOCK_SIZE << '\n' <<
+    cout << "Cache_size: " << cache_size << '\n' <<
+            "Block_size: " << block_size << '\n' <<
             "Hit rate: " << fixed << setprecision(2) << hit_rate*100  << "% (" << hit_num  << "),  " <<
-            "Miss rate: " << miss_rate*100 << "% (" << miss_num << ")\n";
+            "Miss rate: " << miss_rate*100 << "% (" << miss_num << ")\n\n";
     return;
 }
-void memory_access(char C){
-    int hit_num = 0, miss_num = 0;
+void memory_access(const char &C, const unsigned int &cache_size, const unsigned int &block_size){
+    int hit_num = 0, miss_num = 0, block_num = cache_size * 1024 / block_size;
     if(C=='I'){
         vector<element> map(block_num, element());
         for(int i=0; i<I_Cache.size(); i++){
-            unsigned int index = I_Cache[i] % block_num, tag = I_Cache[i] / block_num;
+            unsigned int in = I_Cache[i] / block_size;
+            unsigned int index = in % block_num, tag = in / block_num;
             if(map[index].valid==false){ // compulsory miss
                 map[index].valid = true;
                 map[index].index = index;
@@ -72,7 +70,6 @@ void memory_access(char C){
             }
         }
     
-        cout << "I-Cache Access:\n";
     }
     else if(C=='D'){
         vector<element> map(block_num, element());
@@ -95,16 +92,23 @@ void memory_access(char C){
             }
         }
     
-        cout << "D-Cache Access:\n";
     }
     else{ return; }
-    print_result(hit_num, miss_num);
+    print_result(hit_num, miss_num, cache_size, block_size);
 
 }
 
 int main(void){
     input();
-    memory_access('I');
-    memory_access('D');
+    cout << "-----I-Cache Access-----\n\n";
+    for(int i=0; i<4; i++)
+        for(int j=0; j<5; j++){
+            memory_access('I', cache[i], block[j]);
+        }
+    cout << "-----D-Cache Access-----\n\n";
+    for(int i=0; i<4; i++)
+        for(int j=0; j<5; j++){
+            memory_access('D', cache[i], block[j]);
+        }
     return 0;
 }
