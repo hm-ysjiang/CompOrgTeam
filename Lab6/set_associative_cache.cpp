25b214fb626bb5aef8083bd9bec29a450b5f6c39
hm-ysjiang <hmysjiang.git@gmail.com>
2020-06-27 11:44:28 +0800

    Add csv table output to set_associa

--- Lab6/set_associative_cache.cpp
+++ Lab6/set_associative_cache.cpp
@@ -4,12 +4,15 @@
 #include <vector>
 
 #define BLOCK_SIZE 64
+#define SIZ_SIZE   8
+#define TABLE_OUTPUT
 
 using namespace std;
 
 vector<uint32_t> _data;
-int siz[] = {1, 2, 4, 8, 16, 32, 64},
+int siz[] = {1, 2, 4, 8, 16, 32, 64, 128},
     associa[] = {1, 2, 4, 8};
+float res[SIZ_SIZE][4];
 
 void readData() {
     ifstream in("LRU.txt", ios::in);
@@ -20,7 +23,8 @@ void readData() {
     in.close();
 }
 
-void test(int cache_size_K, int associa_WAY) {
+void test(int _cache_size_K, int _associa_WAY) {
+    int cache_size_K = siz[_cache_size_K], associa_WAY = associa[_associa_WAY];
     int block_count = cache_size_K * 1024 / BLOCK_SIZE;
     int set_count = block_count / associa_WAY;
     vector<vector<uint32_t>> cache(set_count, vector<uint32_t>());
@@ -55,16 +59,36 @@ void test(int cache_size_K, int associa_WAY) {
          << "Cache_size: " << cache_size_K << 'K' << endl
          << "Block_size: " << BLOCK_SIZE << endl
          << "Hit rate: " << fixed << setprecision(2) << hit_rate << "% (" << hit << "), Miss rate: " << (100.0F - hit_rate) << "% (" << miss << ")" << endl;
+    res[_cache_size_K][_associa_WAY] = (float)(miss) / (float)(hit + miss);
 }
 
 int main(void) {
     readData();
     for (int i = 0; i < 4; i++) {
         cout << "-------------------------\n";
-        for (int j = 0; j < 7; j++) {
-            test(siz[j], associa[i]);
+        for (int j = 0; j < SIZ_SIZE; j++) {
+            test(j, i);
         }
         cout << "-------------------------\n";
     }
+
+#ifdef TABLE_OUTPUT
+    ofstream csv("set_asscia.csv", ios::out | ios::binary);
+    csv << fixed << setprecision(4);
+    csv << "/,1K,2K,4K,8K,16K,32K,64K";
+    if (SIZ_SIZE == 8)
+        csv << ",128K";
+    csv << '\n';
+    for (int i = 0; i < 4; i++) {
+        csv << associa[i] << "-way";
+        for (int j = 0; j < SIZ_SIZE; j++) {
+            csv << ',' << res[j][i];
+        }
+        csv << '\n';
+    }
+    csv.flush();
+    csv.close();
+#endif
+
     return 0;
 }
